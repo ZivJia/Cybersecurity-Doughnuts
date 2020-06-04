41bf4e3a5ce126d5d2e2f08dbdcbcf8ef8b47ac2
Ziv Jia <zhiyuan.jia@uber.com>
2020-06-03 22:38:18 -0400

    test git ignore

--- psql-api/src/routes/userRouter.ts
+++ psql-api/src/routes/userRouter.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, Router } from 'express';
 
 let router: Router = express.Router();
 router.route('/').get((req: Request, res: Response) => {
-  res.send('demo user');
+  res.send('demo usessr');
 });
 
 export default router;
