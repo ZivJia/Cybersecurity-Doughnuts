b9117522b0ab7ca6ae6c66a3641a01b1b5f00b09
zhiyuan-jia <zjia@roblox.com>
2024-06-06 23:33:29 -0700

    Adding the draggable Resizable

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="canvas-area">
+    <WindowContainer
+      v-for="(window, windowID) in windows"
+      :key="windowID"
+      :window="window"
+    />
+  </main>
+</template>
+
+<script setup>
+import WindowContainer from './components/WindowContainer.vue'
+
+const windows = {
+  1: {
+    leftPix: 100,
+    topPix: 100,
+    widthPix: 400,
+    heightPix: 300,
+    minWidth: 200,
+    minHeight: 150,
+    zIndex: 10
+  }
+}
+
+</script>
+
+<style lang="scss">
+.canvas-area {
+  height: 100%;
+  width: 100%;
+  right: 0;
+  top: 0;
+  position: relative;
+}
+</style>
---
+++ src/components/WindowContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <VueDraggableResizable
+    :x="window.leftPix || 0"
+    :y="window.topPix || 0"
+    :w="window.widthPix || 100"
+    :h="window.heightPix || 100"
+    :minw="window.minWidth"
+    :minh="window.minHeight"
+    :parent="true"
+    :draggable="true"
+    :resizable="false"
+  >
+    <div
+      :class="{
+        'window-container': true
+      }"
+    />
+  </VueDraggableResizable>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+import VueDraggableResizable from 'vue-draggable-resizable'
+
+const props = defineProps({
+  window: {
+    type: Object,
+    required: true
+  },
+})
+const { window } = toRefs(props)
+
+</script>
+
+<style lang="scss" scoped>
+.window-container {
+  pointer-events: none;
+  height: 100%;
+  width: 100%;
+  position: relative;
+  border-radius: 5px;
+  background-color: rgb(15, 15, 15);
+}
+</style>
